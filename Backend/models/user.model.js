8401ba4f4b536f64f3d8cd58ea509bb3e7ab5b10
MajidKh230 <khanmajid230@gmail.com>
2024-12-13 23:29:37 +0500

    work on  user.model

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,30 +1,51 @@
-import { Schema } from "mongoose";
-
-const useSchema = new Schema({
-    fullname:{
-        firstname:{
-            type:String,
-            required:true,
-            minlength:[3,'First name must be atleast 3 characters long']
-        },
-        lastname:{
-            type:String,
-            minlength:[3,'Last name must be atleast 3 characters long']
-        },
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+const userSchema = new Schema(
+  {
+    fullname: {
+      firstname: {
+        type: String,
+        required: true,
+        minlength: [3, "First name must be atleast 3 characters long"],
+      },
+      lastname: {
+        type: String,
+        minlength: [3, "Last name must be atleast 3 characters long"],
+      },
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-        minlength:[5,'Email must be atleast 5 characters long']
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      minlength: [5, "Email must be atleast 5 characters long"],
     },
-    password:{
-        type:Number,
-        required:true,
-
+    password: {
+      type: Number,
+      required: true,
+      select: false,
     },
-    socketId:{
-        type:String
-    }
+    socketId: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
-},{timestamps:true})
+userSchema.methods.generateAuthToken = () => {
+  const token = jwt.sign({ _id: this.id }, process.env.JWT_SECRET);
+  return token;
+};
+
+userSchema.methods.comparePassword = async () => {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.statics.hashPassword = async ()=>{
+    return await bcrypt.hash(password,10) 
+}
+
+
+const userModel = mongoose.model("user", userSchema);
+
+export default userModel;
