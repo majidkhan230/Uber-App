7ccad326508597c74ad71fd68cb72f85a20dea85
MajidKh230 <khanmajid230@gmail.com>
2024-12-13 21:58:29 +0500

    configur db

---
+++ Backend/db/db.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+async function connectToDb() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("sucessfully connected to db")
+  } catch (error) {
+    console.error({ message: "failed to connect to db", error: error.message });
+  }
+}
