6c5ffa2e80cae33e037198689b42d8b0b4721023
MajidKh230 <khanmajid230@gmail.com>
2024-12-13 21:41:24 +0500

    add server

---
+++ Backend/index.js
@@ -0,0 +1,14 @@
+import express, { urlencoded } from 'express'
+import cors from 'cors'
+
+const app = express();
+
+app.use(express.json())
+app.use(cors)
+
+
+app.get("/",(req,res)=>{
+res.status(200).send({message:"welcome to home"})
+})
+
+export default app
---
+++ Backend/server.js
@@ -0,0 +1,10 @@
+import app from './index.js'
+import dotenv from "dotenv"
+
+dotenv.config()
+
+
+const PORT = process.env.PORT || 8080
+app.listen(PORT,()=>{
+    console.log(`server is sucessfully established on ${PORT}`)
+})
