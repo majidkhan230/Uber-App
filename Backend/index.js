3e7762f70bfdea05eed40e2f3289973c5bf09186
MajidKh230 <khanmajid230@gmail.com>
2024-12-14 09:49:57 +0500

    add user.service

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,25 @@
 import express, { urlencoded } from 'express'
 import cors from 'cors'
+import userRoutes from './routes/user.routes.js';
 
 const app = express();
 
 app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 app.use(cors())
+
+
 
 
 app.get("/",(req,res)=>{
 res.status(200).send({message:"welcome to home"})
 })
+
+
+app.use('/user',userRoutes)
+
+
+
+
 
 export default app
--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import userController from '../controller/user.controller.js';
 import validate from 'express-validator'
-const router = express.Router();
+const userRoutes = express.Router();
 
-router.post("/register",[
+userRoutes.post("/register",[
     validate.body('email').isEmail().withMessage('Invalid Email'),
     validate.body('fullname.firstname').isLength({min:3}).withMessage('First name must be atleast 3 characters long'),
     validate.body('password').isLength({min:6}).withMessage('Password must be atleast 6 characters long')
 ] ,userController)
 
-export default router
+export default userRoutes
